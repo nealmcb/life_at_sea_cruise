9563c99d4d66e27a8a0cda78e24192cbcdb45444
Neal McBurnett <neal@mcburnett.org>
2023-06-03 19:15:41 -0600

    last shot without giving it algorithm

--- segments.py
+++ segments.py
@@ -20,8 +20,8 @@ def summarize_trip(df):
                 ongoing_port['Depart'] = row['Depart']
             else:
                 # finalize the ongoing port visit
-                first_day_hours = 24 - ongoing_port['Arrive'] if ongoing_port['Arrive'] else 24
-                last_day_hours = ongoing_port['Depart'] if ongoing_port['Depart'] else 24
+                first_day_hours = 24 - ongoing_port['Arrive'] 
+                last_day_hours = ongoing_port['Depart']
                 extra_days = (row['Date'] - ongoing_port['Date']).days - 1
                 hours = first_day_hours + last_day_hours + extra_days * 24
                 port_row = {'Date': ongoing_port['Date'], 'Day': ongoing_port['Day'], 'Port': ongoing_port['Port'], 
@@ -29,31 +29,50 @@ def summarize_trip(df):
                 summarized_data.append(port_row)
                 # start a new port visit
                 ongoing_port = row.copy()
-        elif ongoing_port is not None:
-            # finalize the ongoing port visit
-            first_day_hours = 24 - ongoing_port['Arrive'] if ongoing_port['Arrive'] else 24
-            last_day_hours = ongoing_port['Depart'] if ongoing_port['Depart'] else 24
-            extra_days = (row['Date'] - ongoing_port['Date']).days - 1
-            hours = first_day_hours + last_day_hours + extra_days * 24
-            port_row = {'Date': ongoing_port['Date'], 'Day': ongoing_port['Day'], 'Port': ongoing_port['Port'], 
-                        'Country': ongoing_port['Country'], 'Time': ongoing_port['Arrive'], 'Hours': hours}
-            summarized_data.append(port_row)
-            ongoing_port = None
-        
-        if row['Port'] == 'Sea' and (ongoing_port is None or ongoing_port['Port'] != 'Sea'):
+
+        elif row['Port'] == 'Sea' and (ongoing_port is None or ongoing_port['Port'] != 'Sea'):
+            if ongoing_port is not None:
+                # finalize the ongoing port visit
+                first_day_hours = 24 - ongoing_port['Arrive'] 
+                last_day_hours = ongoing_port['Depart']
+                extra_days = (row['Date'] - ongoing_port['Date']).days - 1
+                hours = first_day_hours + last_day_hours + extra_days * 24
+                port_row = {'Date': ongoing_port['Date'], 'Day': ongoing_port['Day'], 'Port': ongoing_port['Port'], 
+                            'Country': ongoing_port['Country'], 'Time': ongoing_port['Arrive'], 'Hours': hours}
+                summarized_data.append(port_row)
             ongoing_port = row.copy()
+
         elif row['Port'] == 'Sea' and ongoing_port['Port'] == 'Sea':
             ongoing_port['Depart'] = row['Depart']
-        elif ongoing_port is not None and ongoing_port['Port'] == 'Sea':
-            # finalize the ongoing sea trip
-            first_day_hours = 24 - ongoing_port['Arrive'] if ongoing_port['Arrive'] else 24
-            last_day_hours = ongoing_port['Depart'] if ongoing_port['Depart'] else 24
-            extra_days = (row['Date'] - ongoing_port['Date']).days - 1
-            hours = first_day_hours + last_day_hours + extra_days * 24
-            sea_row = {'Date': ongoing_port['Date'], 'Day': ongoing_port['Day'], 'Port': 'Sea', 
-                       'Country': 'Sea', 'Time': ongoing_port['Arrive'], 'Hours': hours}
-            summarized_data.append(sea_row)
-            ongoing_port = None
 
     if ongoing_port is not None:
-        first_day_hours = 24
+        first_day_hours = 24 - ongoing_port['Arrive'] 
+        last_day_hours = ongoing_port['Depart']
+        extra_days = (row['Date'] - ongoing_port['Date']).days
+        hours = first_day_hours + last_day_hours + extra_days * 24
+        row = {'Date': ongoing_port['Date'], 'Day': ongoing_port['Day'], 'Port': ongoing_port['Port'], 
+                    'Country': ongoing_port['Country'], 'Time': ongoing_port['Arrive'], 'Hours': hours}
+        summarized_data.append(row)
+
+    summarized_df = pd.DataFrame(summarized_data)
+    return summarized_df
+
+def main(input_file):
+    # Load the csv file
+    df = pd.read_csv(input_file)
+
+    # Process the data
+    summarized_df = summarize_trip(df)
+    
+    # Create output filename and save the dataframe
+    base, ext = os.path.splitext(input_file)
+    output_file = base + "_segments" + ext
+    summarized_df.to_csv(output_file, index=False)
+    print(f"Saved the processed data to {output_file}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='
+    parser.add_argument('input_file', help='The CSV file to process.')
+    args = parser.parse_args()
+
+    main(args.input_file)
